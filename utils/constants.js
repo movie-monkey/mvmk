637beaa3e8ca071feba3ae5f4069da74a5abc4e9
movie-monkey <moviemonkey2024@gmail.com>
2025-11-24 13:59:08 +0200

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
-export const QUERY_MOVIES_ROOT = "https://yts.mx/api/v2/list_movies.json";
-export const MOVIE_DETAILS_ROOT = "https://yts.mx/api/v2/movie_details.json";
+export const QUERY_MOVIES_ROOT = "https://yts.lt/api/v2/list_movies.json";
+export const MOVIE_DETAILS_ROOT = "https://yts.lt/api/v2/movie_details.json";
 export const MOVIE_SUBTITLES_ROOT = "https://movie-monkey-server.fly.dev/movie_subtitles";
